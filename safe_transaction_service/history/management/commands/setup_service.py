ebc7e8f2ac671b7eb5089f05561547b1a0217d5e
Ryan <652456537@qq.com>
2021-11-18 20:21:36 +0800

    Support KCC

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -45,28 +45,28 @@ TASKS = [
     CeleryTaskConfiguration(
         "safe_transaction_service.history.tasks.index_internal_txs_task",
         "Index Internal Txs",
-        13,
+        3,
         IntervalSchedule.SECONDS,
         enabled=not settings.ETH_L2_NETWORK,
     ),
     CeleryTaskConfiguration(
         "safe_transaction_service.history.tasks.index_safe_events_task",
         "Index Safe events (L2)",
-        13,
+        3,
         IntervalSchedule.SECONDS,
         enabled=settings.ETH_L2_NETWORK,
     ),
     CeleryTaskConfiguration(
         "safe_transaction_service.history.tasks.index_new_proxies_task",
         "Index new Proxies",
-        15,
+        3,
         IntervalSchedule.SECONDS,
         enabled=settings.ETH_L2_NETWORK,
     ),
     CeleryTaskConfiguration(
         "safe_transaction_service.history.tasks.index_erc20_events_task",
         "Index ERC20/721 Events",
-        14,
+        3,
         IntervalSchedule.SECONDS,
     ),
     CeleryTaskConfiguration(
@@ -114,6 +114,10 @@ TASKS = [
 ]
 
 MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
+    EthereumNetwork.KCC: [
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 4860807, "1.3.0+L2"),
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 4860810, "1.3.0"),
+    ],
     EthereumNetwork.MAINNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 12504423, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 12504268, "1.3.0"),
@@ -226,6 +230,9 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
+    EthereumNetwork.KCC: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 4860798),  # v1.3.0
+    ],
     EthereumNetwork.MAINNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 12504126),  # v1.3.0
         ("0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B", 9084508),  # v1.1.1
